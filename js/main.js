8026a5c2421c189e2d546e4d176239b531255d3f
LiL <90929523+milkfirst@users.noreply.github.com>
2022-04-10 09:30:35 -0400

    bruh

---
+++ js/main.js
@@ -0,0 +1,15 @@
+var lalaine = document.querySelector('.lil-button');
+var carson = document.querySelector('.car-button');
+
+var textBox = document.querySelector('.teamBio');
+// var img = document.getElementById('img-area');
+
+lalaine.addEventListener("click", function lalaine() {
+	//img.src = 'images/lalaine.png'
+	textBox.innerText = 'Hello! My name is Lalaine and I am so excited for this summer and warm weather. I cannot believe that this semester has finally ended and I am excited for next year because we can finally, HOPEFULLY, go to class in person!';
+});
+
+carson.addEventListener('click', function car() {
+	//img.src = 'images/carson.png'
+	textBox.innerText = 'Hi, I am Carson. I am a hard worker and enjoys coming up with the craziest ideas for products each day, No joke. There is a locked box that contains each of my ideas. But you will never find it.';
+});
